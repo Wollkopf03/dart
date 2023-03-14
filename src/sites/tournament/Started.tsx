0c18ee0477cbcd0bef30c957673956bcae53f399
Wollkopf03 <69468067+Wollkopf03@users.noreply.github.com>
2023-03-14 17:05:13 +0100

    fixes #2

--- src/sites/tournament/Started.tsx
+++ src/sites/tournament/Started.tsx
@@ -3,6 +3,7 @@ import { usePlayers } from '../../hooks/usePlayers'
 import { TournamentType } from '../../hooks/useTournaments'
 import { ClosedGame } from './ClosedGame'
 import { Game } from './Game'
+import { Tree } from './Tree'
 
 type Props = {
 	tournament: TournamentType,
@@ -19,19 +20,20 @@ export function Started({ tournament, setTournament }: Props) {
 			<Typography variant='h5' sx={{ ml: 1, mt: 2 }}>
 				Pending
 			</Typography>
-			{tournament.data.open.map((round, index) =>
-				<Paper key={index} sx={{ p: 1, my: 2 }} variant="outlined">
-					<Grid container spacing={3}>
-						{round.games.map((game, index) => {
-							if (game.players.filter(player => player === -1).length !== 0)
-								return null
-							const player1 = players.filter(player => player.uid === game.players[0])[0]
-							const player2 = players.filter(player => player.uid === game.players[1])[0]
-							return <Game key={game.uid} player1={player1} player2={player2} tournament={tournament} setTournament={setTournament} uid={game.uid} />
-						})}
-					</Grid>
-				</Paper>
-			)}
+			{tournament.data.open.length > 0
+				? tournament.data.open.map((round, index) =>
+					<Paper key={index} sx={{ p: 1, my: 2 }} variant="outlined">
+						<Grid container spacing={3}>
+							{round.games.map((game, index) => {
+								const player1 = players.filter(player => player.uid === game.players[0])[0]
+								const player2 = players.filter(player => player.uid === game.players[1])[0]
+								return <Game key={game.uid} player1={player1} player2={player2} tournament={tournament} setTournament={setTournament} uid={game.uid} />
+							})}
+						</Grid>
+					</Paper>
+				)
+				: <Tree uid={tournament.uid} setTournament={setTournament} />
+			}
 			{tournament.data.closed.length > 0 && <>
 				<Typography variant='h5' sx={{ ml: 1, my: 2 }}>
 					Finished
